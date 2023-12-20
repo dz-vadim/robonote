8fbe8674c86a3067d68050b69ae9c7c8c53fdb37
dz-vadim <dziuba.vadym@gmail.com>
2023-12-20 04:58:56 +0200

    Cleaned all

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import UserList from "./components/UserList";
-import TodoList from "./components/TodoList";
+
 
 const App = () => {
     return (
         <div>
-          <UserList/>
-            <hr/>
-            <TodoList/>
+            Work
         </div>
     );
 };
--- src/hooks/useAction.ts
+++
@@ -1,7 +0,0 @@
-import {useDispatch} from "react-redux";
-import {bindActionCreators} from "redux";
-import ActionCreators from '../store/action-creators/'
-export const useAction = () => {
-    const dispatch = useDispatch()
-    return bindActionCreators(ActionCreators, dispatch)
-}
